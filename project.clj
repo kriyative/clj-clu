320f481a75c79f9acd17cb70f4d5e3d9eb44dea6
Ram Krishnan <kriyative@gmail.com>
2012-11-21 16:41:20 -0800

    Setup for clojars deploy.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clj-clu "1.0.0"
+(defproject org.clojars.kriyative/clj-clu "1.0.0"
   :description "Generic command line utility for Clojure"
-  :plugins [[lein-swank "1.4.4"]]
+  :plugins [[lein-swank "1.4.4"]
+            [lein-clojars "0.9.1"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]])
